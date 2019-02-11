ebac55e8a428c8a1b34584a025c76ce3b37c3633
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-11 16:16:05 -0500

    Completed building out all nested components for HeaderContainer and CardContainer

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent () {
+    return (
+        <div>
+            <p class="get-started-card-content">Get started with React</p>
+            <p class="text-card-content">
+                React makes it painless to create interactive UIs. Design
+                simple views for each state in your application.
+            </p>
+            <p class="react-url">reactjs.org</p>
+        </div>
+    );
+}
+
+export default CardContent;
